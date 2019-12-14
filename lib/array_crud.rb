a187e45a752ba9594042fd694b937b3fcb1f6077
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-14 18:18:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["red", "gold", "black", "purple"]
+  array = ["red", "gold", "black", "arrays!"]
   array.pop
 end
 
