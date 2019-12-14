6773826a214fa964af6cb3d33fc24fc452078ab3
Anthony Ybarra <anthony_jy72@yahoo.com>
2019-12-14 18:14:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,12 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = ["red", "gold", "black"]
-  array.push 
+  array.push("green") 
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["red", "gold", "black"]
+  array.unshift("blue")
 end
 
 def remove_element_from_end_of_array(array)
